5f7a592e5e076deffe88dbbf639fc47557f440bd
Diego Orellana <dev@dorellango.cl>
2020-02-09 18:47:15 -0300

    adding trayecto crud

--- components/TrayectoCard.vue
+++ components/TrayectoCard.vue
@@ -10,9 +10,31 @@
         v-text="trayecto.terminal"
       ></p>
     </div>
-    <div class="w-full bg-indigo-600 text-indigo-100 py-2 px-6 tracking-wide text-lg">
-      <p class="mb-1 text-xs text-indigo-300">Destino:</p>
-      <p>{{ trayecto.vuelta }}</p>
+    <div
+      class="w-full bg-indigo-600 text-indigo-100 py-2 px-6 tracking-wide text-lg flex justify-between"
+    >
+      <div class="mr-2">
+        <p class="mb-1 text-xs text-indigo-300">Retorno:</p>
+        <p>{{ trayecto.vuelta }}</p>
+      </div>
+      <!-- Destroy -->
+      <button
+        class="text-indigo-400 hover:text-indigo-300 focus:outline-none"
+        @click="$emit('destroy', trayecto)"
+      >
+        <svg
+          class="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"
+          />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+      </button>
     </div>
   </div>
 </template>
--- pages/trayectos.vue
+++ pages/trayectos.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mx-auto">
-    <div class="flex items-center justify-between border-b border-dashed border-gray-300 py-4 mb-8">
+    <div
+      class="flex items-center justify-between border-b border-dashed border-gray-300 py-4 mb-8"
+    >
       <div class="flex items-center">
         <h1 class="text-2xl text-gray-800 mr-2">Trayectos</h1>
         <p
@@ -8,9 +10,65 @@
           v-text="trayectos.length"
         ></p>
       </div>
-      <button class="btn-indigo">Ingresar</button>
+      <button @click="$modal.show('trayecto-add')" class="btn-indigo">
+        Ingresar
+      </button>
     </div>
-    <trayecto-card v-for="trayecto in trayectos" :key="trayecto.id" :trayecto="trayecto"></trayecto-card>
+    <!-- Trayectos -->
+    <transition-group name="fade" tag="div">
+      <trayecto-card
+        v-for="trayecto in trayectos"
+        :key="trayecto.id"
+        :trayecto="trayecto"
+        @destroy="destroy"
+      ></trayecto-card>
+    </transition-group>
+
+    <!-- Add Chofer -->
+    <modal name="trayecto-add" height="auto" :scrollable="true">
+      <div class="p-4 text-center bg-gray-200">
+        <h1 class="text-lg font-bold text-gray-700">Ingreso Chofer</h1>
+      </div>
+      <form @submit.prevent="add">
+        <!-- @keydown.enter="add" -->
+        <div class="px-4 py-6">
+          <input-form
+            v-model="form.ida"
+            class="mb-2"
+            label="ida"
+            name="ida"
+            type="date"
+            placeholder="DD/MM/YYYY"
+            :required="true"
+          ></input-form>
+          <input-form
+            v-model="form.vuelta"
+            class="mb-2"
+            label="vuelta"
+            name="vuelta"
+            type="date"
+            placeholder="DD/MM/YYYY"
+            :required="true"
+          ></input-form>
+          <input-form
+            v-model="form.terminal"
+            class="mb-2"
+            label="terminal"
+            name="terminal"
+            type="text"
+            placeholder="Colon"
+            :required="true"
+          ></input-form>
+        </div>
+        <div class="p-4 bg-gray-200 flex">
+          <button type="submit" class="btn-indigo-o">Guardar</button>
+          <button class="btn-default" @click="$modal.hide('trayecto-add')">
+            Cerrar
+          </button>
+        </div>
+      </form>
+    </modal>
+    <!-- Add Chofer -->
   </div>
 </template>
 
@@ -20,21 +78,76 @@ import TrayectoCard from "~/components/TrayectoCard";
 export default {
   data() {
     return {
-      trayectos: [
-        {
-          id: 1,
-          ida: "Valparaiso",
-          vuelta: "Santiago",
-          terminal: "San Borja"
-        },
-        {
-          id: 2,
-          ida: "Valparaiso",
-          vuelta: "Puerto Montt",
-          terminal: "Colón"
-        }
-      ]
+      form: {
+        ida: "",
+        vuelta: "",
+        terminal: ""
+      }
     };
+  },
+  async asyncData({ $axios }) {
+    const trayectos = await $axios.$post("/trayecto/all", {
+      user: "diego_orellana",
+      pass: "destacameorellana"
+    });
+
+    return { trayectos };
+  },
+  methods: {
+    async destroy({ id }) {
+      // if (this.trayectos.some(c => c.id === id)) {
+      //   return this.$vToastify.error(
+      //     "No puedes eliminar choferes con buses asignados",
+      //     "Nope!"
+      //   );
+      // }
+      try {
+        await this.$axios.delete(`/trayecto/${id}`, {
+          data: {
+            user: "diego_orellana",
+            pass: "destacameorellana"
+          }
+        });
+        this.removeTrayecto(id);
+        this.$vToastify.info("Trayecto eliminado exitósamente 😢", "¡Hecho!");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async add() {
+      try {
+        await this.$axios.post("/trayecto", {
+          user: "diego_orellana",
+          pass: "destacameorellana",
+          ...this.form
+        });
+        this.reset();
+        this.updateList();
+        this.$vToastify.success("Trayecto agregado exitósamente 😄", "¡Hecho!");
+        this.$modal.hide("trayecto-add");
+      } catch (error) {
+        console.error("Something goes wrong");
+      }
+    },
+    reset() {
+      this.form.ida = "";
+      this.form.vuelta = "";
+      this.form.terminal = "";
+    },
+    async updateList() {
+      const { data } = await this.$axios.post("/trayecto/all", {
+        user: "diego_orellana",
+        pass: "destacameorellana"
+      });
+      this.trayectos = data;
+    },
+    removeTrayecto(id) {
+      const { trayectos } = this;
+      trayectos.splice(
+        trayectos.findIndex(c => c.id === id),
+        1
+      );
+    }
   },
   components: { TrayectoCard }
 };

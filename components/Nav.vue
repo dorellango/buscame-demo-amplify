29d2ac15f7cb8291422715f09ab0506e4aa2a9f3
Diego Orellana <dev@dorellango.cl>
2020-02-11 17:42:38 -0300

    adding logo and remove amplify as framework

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,24 +2,7 @@
   <div class="bg-indigo-800 text-blue-100 shadow-md border-b border-indigo-900">
     <div class="container mx-auto px-4">
       <div class="flex items-center py-4 h-16">
-        <div class="mr-auto flex items-center">
-          <svg
-            class="text-indigo-100 h-8 w-8 mr-1 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="-4 -2 24 24"
-            width="24"
-            height="24"
-            preserveAspectRatio="xMinYMin"
-          >
-            <path
-              d="M4 2a2 2 0 0 0-2 2v5l12-.037V4a2 2 0 0 0-2-2H4zm7 15H5v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-2.535A3.998 3.998 0 0 1 0 13V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v9c0 1.48-.804 2.773-2 3.465V19a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-2zm-7-3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm8 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zM4 3a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1z"
-            />
-          </svg>
-          <p class="text-3xl">
-            bus
-            <span class="font-bold -ml-1">.CO</span>
-          </p>
-        </div>
+        <logo></logo>
         <!-- Links -->
         <div class="ml-2 flex items-center">
           <!-- Mobile -->
@@ -80,9 +63,10 @@
 
 <script>
 import NavMobile from "~/components/NavMobile";
+import Logo from "~/components/Logo";
 
 export default {
-  components: { NavMobile }
+  components: { Logo }
 };
 </script>
 
